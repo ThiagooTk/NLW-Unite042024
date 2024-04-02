d1b538da090ffc0bf4900488563259c304121069
Thiago Lacerda Donolato <tlacerda0503@gmail.com>
2024-04-01 22:22:07 -0300

    feat: Implementing events_repository

---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,25 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Pular insert")
+def test_insert_event():
+  event = {
+    "uuid": "xxx-uuid2",
+    "title": "meu_title",
+    "slug": "meu_slug2",
+    "maximum_attendees": 20
+  }
+
+  event_repository = EventsRepository()
+  response = event_repository.insert_event(event)
+  print(response)
+
+#@pytest.mark.skip(reason="Pular query")
+def teste_get_event_by_id():
+  event_id = "xxx-uuid"
+  event_repository = EventsRepository()
+  response = event_repository.get_event_by_id(event_id)
+  print(response)
